2d40912d58e195d2f682f6513ebfc7dc569758af
Ertas10 <nuno10_rayman.crash@live.com.pt>
2018-12-10 15:59:33 +0000

    Commit

--- app/src/main/java/com/mypokebase/MainActivity.java
+++ app/src/main/java/com/mypokebase/MainActivity.java
@@ -1,19 +1,30 @@
 package com.mypokebase;
 
 import android.os.storage.StorageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
+    DatabaseReference dataRef;
+    String value;
+    Map<String, String> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void GetDatabase(){
+        database = FirebaseDatabase.getInstance();
+        dataRef = database.getReference();
+        dataRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                data = (Map<String, String>) dataSnapshot.getValue();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
